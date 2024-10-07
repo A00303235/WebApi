ba0166015834dad423858f691b24e49fd7664e1a
A00303235 <A00303235@mycambrian.ca>
2024-10-07 15:50:45 -0400

    Codded files, Registered the services and made a new controller

--- WebApi/Interfaces/ICalculatorService.cs
+++ WebApi/Interfaces/ICalculatorService.cs
@@ -1,6 +1,10 @@
 ﻿namespace WebApi.Interfaces
 {
-    public interface ICalculatorService
+    public interface ICalculatorService<T>
     {
+        T Add(T a, T b);
+        T Subtract(T a, T b);
+        T Multiply(T a, T b);
+        T Divide(T a, T b);
     }
 }
--- WebApi/Services/CalculatorService.cs
+++ WebApi/Services/CalculatorService.cs
@@ -1,6 +1,12 @@
-﻿namespace WebApi.Services
+﻿using WebApi.Interfaces;
+
+namespace WebApi.Services
 {
-    public class CalculatorService
+    public class CalculatorService : ICalculatorService<double>
     {
+        public double Add(double a, double b) => a + b;
+        public double Subtract(double a, double b) => a - b;
+        public double Multiply(double a, double b) => a * b;
+        public double Divide(double a, double b) => a / b;
     }
 }
